5908f497aa42544082cc502d8232dee3b8ede025
seank42 <N00211599@iadt.ie>
2023-10-12 12:19:45 +0100

    first commit

--- src/components/ToDoitem.js
+++ src/components/ToDoitem.js
@@ -1,13 +1,40 @@
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 
-const ToDoitem = (props) => {
+const TodoItem = (props) => {
+  let itemHTML = (
+    <>
+      {props.todo.text}
 
-    return(
-        <>
-        <ListGroup.Item>{props.todo.text}</ListGroup.Item>
-        </>
+      <Badge
+        onClick={() => props.markAsDone(props.todo.id)}
+        pill
+        bg="success"
+        className="float-end"
+      >
+        &#10003;
+      </Badge>
+    </>
+  );
+
+  if (props.todo.done) {
+    itemHTML = (
+      <>
+        <span className="done">{props.todo.text}</span>
+
+        <span className="float-end">👽</span>
+      </>
     );
+  }
 
+  return (
+    <>
+      <ListGroup.Item>{itemHTML}
+      <Button onClick={()=> props.deleteToDo(props.todo.id)} className="float-end" variant="danger" size="sm">Delete </Button>
+      </ListGroup.Item>
+    </>
+  );
 };
 
-export default ToDoitem ;
+export default TodoItem;
--- src/components/ToDolist.js
+++ src/components/ToDolist.js
@@ -1,44 +1,109 @@
 import Card from "react-bootstrap/Card";
+
 import Button from "react-bootstrap/Button";
+
 import ListGroup from "react-bootstrap/ListGroup";
-import ToDoitem from "./ToDoitem";
-import { useState } from "react";
 
+import { useEffect, useState } from "react";
 
-const ToDolist = () => {
- let intiallist = [
-    {id:1 , text:`clean the place`, done:true},
-    {id:2 , text:`clean place`, done:false},
-    {id:2 , text:` place`, done:true}
-];
+import TodoItem from "./ToDoItem";
 
-const [list, setList] = useState(intiallist);
-const [textInput, setTextInput] = useState(''); 
+const TodoList = () => {
+  let initialist = [
+    { id: 1, text: "Clean the office", done: true },
 
-const handleTextInput = (e) => {
+    { id: 2, text: "Eat ceral", done: false },
+
+    { id: 3, text: "Do React shtuff", done: false },
+  ];
+
+  let localList = JSON.parse(localStorage.getItem(`todos`));
+
+  if(localList){
+    initialist = localList;
+    }
+ 
+
+  const [list, setList] = useState(initialist);
+
+  const [textInput, setTextInput] = useState("");
+
+  useEffect(()=>{
+   localStorage.setItem(`todos`,JSON.stringify(list))
+  },[list])
+
+  const handleTextInput = (e) => {
     setTextInput(e.target.value);
-};
+  };
 
-let todoItems = list.map((item) => {
-return <ToDoitem key={item.id} todo={item}/>
-});
+  const addTodoItem = () => {
+   let lastItem = list[list.length -1 ];
+   let id = 1
+   
+   if(lastItem){
+id = lastItem.id + 1  ;  
+}
+    let newTodo = {
+      id: id ,
+      text: textInput,
+       done: false,
+    };
+
+    setList((prevList) => [...prevList, newTodo]);
+
+    setTextInput("");
+  };
+
+
+
+  const markAsDone = (id) => {
+    const newList = list.map((item) => {
+      if (item.id === id) {
+        item.done = true;
+      }
+
+      return item;
+    });
+
+    setList(newList);
+  };
+
+  const deleteToDo = (id) => {
+    const newlist = list.filter((item)=>{
+      return item.id !== id;
+    });
+    setList(newlist)
+  }
+
+  const handleKeyUP =(e) => {
+    if(e.key === 'Enter'){
+    addTodoItem();
+    }
+    };
+
+  const getOutLad = () => {};
+
+  const todoItems = list.map((item) => {
+    return <TodoItem key={item.id} todo={item} markAsDone={markAsDone} deleteToDo={deleteToDo}/>;
+  });
 
   return (
     <Card>
-      <Card.Header>ToDolist</Card.Header>
+      <Card.Header>To Do List</Card.Header>
 
       <Card.Body>
-        <ListGroup>
-          {todoItems}
-        </ListGroup>
+        <ListGroup>{todoItems}</ListGroup>
       </Card.Body>
 
       <Card.Footer>
-        <input type='text' onChange={handleTextInput} value={textInput}/>
-        <Button variant="primary" className="float-end"> Add</Button>
+        <input type="text" onChange={handleTextInput} value={textInput} onKeyUp={handleKeyUP} />
+
+        <Button variant="primary" className="float-end" onClick={addTodoItem}>
+          Add
+        </Button>
       </Card.Footer>
     </Card>
   );
 };
 
-export default ToDolist;
+export default TodoList;
